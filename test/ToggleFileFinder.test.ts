4b5a2a788095623fb2438c96abc04d85e612bcd2
HOMMA Takashi <hommatks@gmail.com>
2016-10-11 22:49:46 +0900

    fix whitespace

--- test/ToggleFileFinder.test.ts
+++ test/ToggleFileFinder.test.ts
@@ -1,6 +1,6 @@
 import * as assert from 'assert';
 
- import * as vscode from 'vscode';
+import * as vscode from 'vscode';
 import * as ToggleFileFinder from '../src/ToggleFileFinder';
 
 class FakeBehavior implements ToggleFileFinder.ToggleFileFinderBehavior {
